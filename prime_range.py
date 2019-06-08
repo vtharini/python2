8709beedfa20c47ff082528816ab7decc217cb9e
vtharini <46223387+vtharini@users.noreply.github.com>
2019-06-08 21:50:10 +0530

    Create prime_range.py

---
+++ prime_range.py
@@ -0,0 +1,13 @@
+import math
+l,u=input().split()
+prime=[]
+for i in range(int(l),int(u)+1):
+    prime.append(i)
+i=2
+while(i <= int(math.sqrt(int(u)))):
+    if i in prime:
+        for j in range(i*2, int(u)+1, i):
+            if j in prime:
+                prime.remove(j)
+    i = i+1
+print(prime)
