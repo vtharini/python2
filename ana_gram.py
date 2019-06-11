b5e0da163cbb4976b1bc66b1aa9df36900cbe00e
vtharini <46223387+vtharini@users.noreply.github.com>
2019-06-11 22:34:51 +0530

    Update ana_gram.py

--- ana_gram.py
+++ ana_gram.py
@@ -1,9 +1,9 @@
-l1=['k','a','b','a','l','i']
+l=['k','a','b','a','l','i']
 def check(st):
     lis=list(st)
     for i in lis:
-        if(i in l1):
-            if(l1.count(i)<=lis.count(i)):
+        if(i in l):
+            if(l.count(i)<=lis.count(i)):
                 continue
             else:
                 return 0
@@ -14,12 +14,12 @@ def check(st):
     else:
         return 1
 n=int(input())
-l2=[]
+l1=[]
 c=0
 for i in range(n):
     s=input()
-    l2.append(s)
-for i in l2:
+    l1.append(s)
+for i in l1:
     r=check(i)
     if(r==1):
         c+=1
