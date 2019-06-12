83c039a03eefd4a5f0a6312c91361c80d80b7618
vtharini <46223387+vtharini@users.noreply.github.com>
2019-06-12 19:41:22 +0530

    Update insertion_max.py

--- insertion_max.py
+++ insertion_max.py
@@ -1,6 +1,7 @@
 n,k=map(int,input().split())
+d=input()
 l1=list(map(int,input().split()))
 l2=list(map(int,input().split()))
 for i in l2:
     l1.append(i)
-    print(max(l1))
+    print(max(l1),end=" ")
