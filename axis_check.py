59ef0c5d2abcb2ac6d8de29be4c9769348a245b0
vtharini <46223387+vtharini@users.noreply.github.com>
2019-06-12 18:34:53 +0530

    Create axis_check.py

---
+++ axis_check.py
@@ -0,0 +1,9 @@
+p,p1=map(int,input().split())
+p2,p3=map(int,input().split())
+p4,p5=map(int,input().split())
+if(p==p2 and p2==p4):
+    print("yes")
+elif(p1==p3 and p3==p5):
+    print("yes")
+else:
+    print("no")
