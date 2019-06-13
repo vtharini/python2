72f31076173caa339af08b4419959f60c7e18dfb
vtharini <46223387+vtharini@users.noreply.github.com>
2019-06-13 16:44:06 +0530

    Create sort_string.py

---
+++ sort_string.py
@@ -0,0 +1,7 @@
+w=int(input())
+l=list(map(str,input().split()))
+l1=sorted(l,key=len)
+for i in range(w-1):
+    if(len(l1[i])==len(l1[i+1]) and l1[i]>l1[i+1]):
+        l1[i],l1[i+1]=l[i+1],l[i]
+print(*l1)
