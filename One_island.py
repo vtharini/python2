81463258a91c9b5ac8122273b6502fe5d9f2fc54
vtharini <46223387+vtharini@users.noreply.github.com>
2019-06-17 17:27:10 +0530

    Update One_island.py

--- One_island.py
+++ One_island.py
@@ -12,5 +12,5 @@ l.append([0,0,0,0])
 for i in range(1,nu+1):
     for j in range(1,nu+1):
         if(l[i][j-1]==l[i][j+1]==l[i-1][j]==l[i-1][j]==0):
-            c+=1
+            cnt+=1
 print(cnt)
